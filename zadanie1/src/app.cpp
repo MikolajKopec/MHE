76f7540de5ba9a4effb71017e16af2516b0f9cd6
Mikołaj Kopeć <s23440@pjwstk.edu.pl>
2022-10-10 12:25:22 +0200

    x

--- zadanie1/src/app.cpp
+++ zadanie1/src/app.cpp
@@ -7,10 +7,8 @@
 int main(){
     std::ifstream file("window_settings.json");
     nlohmann::json data = nlohmann::json::parse(file);
-    
     for(auto i:data){
         std::cout<<i<<std::endl;
     }
-
     return 0;
 }
